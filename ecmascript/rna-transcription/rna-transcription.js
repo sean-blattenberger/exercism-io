dddcb349aa1bdb53b0495ec5631c9c63d9cb39eb
sean-blattenberger <sean.blattenberger@gmail.com>
2017-09-12 19:01:57 -0400

    Completed and submitted rna-transcription

--- ecmascript/rna-transcription/rna-transcription.js
+++ ecmascript/rna-transcription/rna-transcription.js
@@ -1,17 +1,29 @@
 
 class Transcriptor {
-  constructor(dna) {
-    this.dna = dna;
-  }
-  toRna() {
-    let dnaComplements = {
-      g: 'c',
-      c: 'g',
-      t: 'a',
-      a: 'u'
-    }
-    return dnaComplements[this.dna];
-  }
 
+  toRna(dna) {
+    let dnaComplements = {
+      'G': 'C',
+      'C': 'G',
+      'T': 'A',
+      'A': 'U'
+    }
+
+
+    //Error function for invalid input
+    function _throwError() {
+      throw new Error('Invalid input DNA.');
+    }
+
+    /*
+    * Ternary conditional: if argument contains any letter outside of the valid dna inputs,
+    * throw an error. Else run map function and return rna compliments
+    */
+    return dna.match(/[^GCTA]/g) != null ? _throwError() : dna.split('').map(function (e, i) {
+      return dnaComplements[e];
+    }).join('');
+
+  }
 }
+
 export default Transcriptor;
