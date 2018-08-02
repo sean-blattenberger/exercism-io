dc53d97c2e3cb56dee1f358ed86b7986acc19d28
sean-blattenberger <sean.blattenberger@gmail.com>
2018-08-01 19:00:44 -0600

    Added sieve solution

--- ecmascript/sieve/sieve.js
+++ ecmascript/sieve/sieve.js
@@ -1,5 +1,25 @@
 export default class Sieve {
-  constructor(num) {
-    this.num = num;
+  constructor(range) {
+    this.range = range;
+    this.primes = this.generatePrimes();
+  }
+  generatePrimes() {
+    let curr = 2;
+    let primes = [];
+    let composites = {};
+    while (curr <= this.range) {
+      if(!composites[curr]){
+        for (let i = curr + 1; i <= this.range; i++) {
+          if(i % curr == 0) composites[i] = i;
+        }
+      }
+      curr++;
+    }
+    // console.log(composites);
+    for (let j = 2; j <= this.range; j++) {
+      const element = j;
+      if(!composites[element]) primes.push(element);
+    }
+    return primes;
   }
 }
